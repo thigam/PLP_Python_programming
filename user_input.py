8906932be91f87321cbd988da3e1d89ce4254aa9
thigam <matthewthiga72@gmail.com>
2024-02-28 22:55:54 +0100

    First push

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Kindly enter your name: ")
+age = input("Kindly enter your age: ")
+location = input("Kindly enter your location: ")
+print("Hello {}, you are {} years old and live in {}".format(name, age, location))
